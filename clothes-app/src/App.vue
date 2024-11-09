8a0c92f3799f228a62457cd2767e4420e9b5f175
grochalapiotr <miami2042@gmail.com>
2024-11-09 15:46:55 +0100

    add firebase configuration

--- clothes-app/src/App.vue
+++ clothes-app/src/App.vue
@@ -1,5 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+import { db } from "./firebaseConfig";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+
+async function addUser(name, email) {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      name: name,
+      email: email,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function getUsers() {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => `, doc.data());
+  });
+}
+
+getUsers();
 </script>
 
 <template>
