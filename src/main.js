bb5846c62e53b927063ba846faaa90d8b784df38
grochalapiotr <miami2042@gmail.com>
2024-12-17 17:18:26 +0100

    minor changes v5

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
 // import './assets/styles.css'
-import './style.css'
+import "./style.css";
 
 // createApp(App).mount('#app')
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
